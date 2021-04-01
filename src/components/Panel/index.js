debbe05321eb32c41d60248ea8b85bd81ed225f7
Hazieon <handrew3721@gmail.com>
2021-04-02 00:25:43 +0100

    FEATURES and STYLES - useReducer hook added to store state. Page 1 setup

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,10 +1,57 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import styles from "./index.module.css";
+import Services from "../Services";
+
+const serviceOptions = [
+  "Web Design & Build",
+  "Pay Per Click (PPC)",
+  "Search Engine Optimisation (SEO)",
+  "Paid Social",
+  "Digital Marketing",
+];
+//array which holds the page numbers available to iterate through:
+const componentPages = [0, 1, 2];
+
+const initialState = {
+  service: "",
+  type: "",
+  industry: "",
+  budget: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setService":
+      return {
+        ...state,
+        service: action.service,
+      };
+    case "setType":
+      return {
+        ...state,
+        type: action.serviceType,
+      };
+    case "setIndustry":
+      return {
+        ...state,
+        type: action.industry,
+      };
+    case "setBudget":
+      return {
+        ...state,
+        type: action.budget,
+      };
+
+    default:
+      throw new Error();
+  }
+}
 
 function Panel() {
+  // state to set and hold value of the current page
   const [currentPanel, setCurrentPanel] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const componentPages = [0, 1, 2];
   function incComponent() {
     //array with order of pages, move to NEXT index on button click
     if (currentPanel < componentPages.length) {
@@ -21,8 +68,24 @@ function Panel() {
     }
   }
 
+  function serviceSetter(e) {
+    dispatch({ type: "setService", service: e.target.value });
+  }
+
   return (
     <div className={styles.container}>
+      <div className={styles.progressBar}>
+        <div className={styles.progress}></div>
+      </div>
+      {currentPanel === 0 && (
+        <section className={styles.servicesSection}>
+          <Services options={serviceOptions} serviceSetter={serviceSetter} />
+        </section>
+      )}
+      <h4>So you're looking for:</h4>
+      <h3 className={styles.selectionText}>
+        {state.service ? state.service : ""}
+      </h3>
       <div className={styles.backButtonContainer}>
         <button className={styles.backButton}>Back</button>
       </div>
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,7 +1,30 @@
 import React from "react";
 import styles from "./index.module.css";
 
-function Services() {
-  return <></>;
+function Services({ options, serviceSetter }) {
+  return (
+    <>
+      <h3 className={styles.title}>What type of service is required?</h3>
+      <div></div>
+      <select
+        onChange={(event) => {
+          console.log(event.target.value);
+          event.persist();
+          serviceSetter(event);
+        }}
+      >
+        <option value="" selected disabled>
+          Select one that applies:
+        </option>
+        {options.map((item) => {
+          return (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </>
+  );
 }
 export default Services;
