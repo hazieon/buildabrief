e8408b116ff2368a417a16e260b633de7f801ae3
Hazieon <handrew3721@gmail.com>
2021-04-02 00:43:11 +0100

    FEATURE - next and back buttons, working progress bar

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -2,6 +2,7 @@ import React, { useReducer, useState } from "react";
 import styles from "./index.module.css";
 import Services from "../Services";
 
+//hard coded list of available services within Columba
 const serviceOptions = [
   "Web Design & Build",
   "Pay Per Click (PPC)",
@@ -19,6 +20,7 @@ const initialState = {
   budget: 0,
 };
 
+//reducer function holding the cases for changing state in a controlled way
 function reducer(state, action) {
   switch (action.type) {
     case "setService":
@@ -51,6 +53,7 @@ function Panel() {
   // state to set and hold value of the current page
   const [currentPanel, setCurrentPanel] = useState(0);
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [progress, setProgress] = useState(25);
 
   function incComponent() {
     //array with order of pages, move to NEXT index on button click
@@ -68,14 +71,40 @@ function Panel() {
     }
   }
 
+  function advanceProgressBar() {
+    // if not complete, ADD 1/4 progress, called on NEXT button press
+    if (progress < 100) {
+      setProgress(progress + 25);
+    }
+  }
+
+  function retractProgressBar() {
+    // if not empty, SUBTRACT 1/4 progress, called on BACK button press
+    if (progress > 25) {
+      setProgress(progress - 25);
+    }
+  }
+
   function serviceSetter(e) {
     dispatch({ type: "setService", service: e.target.value });
   }
+  //   function serviceTypeSetter(e) {
+  //     dispatch({ type: "setService", serviceType: e.target.value });
+  //   }
+  //   function IndustrySetter(e) {
+  //     dispatch({ type: "setIndustry", industry: e.target.value });
+  //   }
+  //   function budgetSetter(e) {
+  //     dispatch({ type: "setBudget", budget: e.target.value });
+  //   }
 
   return (
     <div className={styles.container}>
       <div className={styles.progressBar}>
-        <div className={styles.progress}></div>
+        <div
+          className={styles.progress}
+          style={{ width: progress + "%" }}
+        ></div>
       </div>
       {currentPanel === 0 && (
         <section className={styles.servicesSection}>
@@ -87,10 +116,24 @@ function Panel() {
         {state.service ? state.service : ""}
       </h3>
       <div className={styles.backButtonContainer}>
-        <button className={styles.backButton}>Back</button>
+        <button
+          className={styles.backButton}
+          onClick={() => {
+            retractProgressBar();
+          }}
+        >
+          Back
+        </button>
       </div>
       <div className={styles.nextButtonContainer}>
-        <button className={styles.nextButton}>Next</button>
+        <button
+          className={styles.nextButton}
+          onClick={() => {
+            advanceProgressBar();
+          }}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
